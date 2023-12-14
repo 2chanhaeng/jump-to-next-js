ca5e5a44dcf9ac41626ee4618f99ef2e6bdab9f4
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-15 06:04:15 +0900

    04. Client Components.md 작성

---
+++ app/count/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+export default function CountPage() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+    </div>
+  );
+}
