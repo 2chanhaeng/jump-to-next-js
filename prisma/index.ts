ab1f9463b4b7854bd8752ebcaab66bc63f859a0c
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-16 02:06:09 +0900

    @/prisma 에서 Question, Answer 내보내기

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 export { Prisma } from "@prisma/client";
+export type { Question, Answer } from "@prisma/client";
 
 export const prisma = new PrismaClient();
