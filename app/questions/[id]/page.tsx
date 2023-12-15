0d9790da9e721ca7ed137d3bc04346ba39986b26
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-15 22:21:51 +0900

    질문 상세 페이지 생성

---
+++ app/questions/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { prisma } from "@/prisma";
+import { notFound } from "next/navigation";
+
+export default async function QuestionPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const question = await prisma.question.findUnique({
+    where: { id },
+  });
+  if (!question) return notFound();
+  const { subject, content, createdAt } = question;
+
+  return (
+    <div>
+      <h1>{subject}</h1>
+      <p>{content}</p>
+      <p>{createdAt.toString()}</p>
+      <p>
+        {Intl.DateTimeFormat("ko-KR", {
+          dateStyle: "short",
+          timeStyle: "short",
+        }).format(createdAt)}
+      </p>
+    </div>
+  );
+}
