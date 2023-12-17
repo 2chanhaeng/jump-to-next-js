21f03add2611c0716b755064789311807f00d9f5
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-17 20:11:29 +0900

    Credentials 로그인 방식 추가

--- app/auth/[...nextauth]/route.ts
+++ app/auth/[...nextauth]/route.ts
@@ -1,5 +1,42 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { prisma } from "@/prisma";
 
 export const { GET, POST } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        const { username, password } = credentials;
+        if (typeof username !== "string" || typeof password !== "string")
+          return null;
+        const credential = await prisma.credential.findUnique({
+          where: { credentials: { username, password } },
+          select: { id: true },
+        });
+        if (!credential) return null;
+        const account = await prisma.account.findUnique({
+          where: {
+            provider_providerAccountId: {
+              providerAccountId: credential.id,
+              provider: "credentials",
+            },
+          },
+          select: { user: { select: { id: true } } },
+        });
+        return account?.user ?? null;
+      },
+    }),
+  ],
 }).handlers;
