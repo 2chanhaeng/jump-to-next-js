6fef93711ebd9fa7ef08b5615d817f8c13a66c14
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-17 20:11:10 +0900

    임시 계정 데이터 생성

--- dummy.ts
+++ dummy.ts
@@ -3,30 +3,26 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const id = "18cc7998-1321-48f4-b877-e5717c72a0b4";
-  await prisma.answer.deleteMany({ where: { questionId: id } });
-  const createInputs: Prisma.AnswerCreateInput[] = [
-    {
-      content: "Next.js 는 Node.js 를 기반으로 만들어진 SSR 프레임워크 입니다.",
-      question: { connect: { id } },
-    },
-    {
-      content: "Next.js 는 파일 기반 라우팅, 서버 액션 등의 기능이 있습니다.",
-      question: { connect: { id } },
-    },
-    {
-      content: "Next.js 는 React Hooks 등 CSR 기능도 쓸 수 있습니다.",
-      question: { connect: { id } },
-    },
-  ];
-  for (const data of createInputs) {
-    await prisma.answer.create({ data });
-  }
-  const question = await prisma.question.findUnique({
-    where: { id },
-    select: { answers: { select: { content: true } } },
-  });
-  console.log(question?.answers.map(({ content }) => content).join("\n"));
+  const userData: Prisma.UserCreateInput = {
+    email: "qwe@qwe.qwe",
+    name: "User Name",
+  };
+  const user = await prisma.user.create({ data: userData });
+  console.log(user);
+  const credentialData: Prisma.CredentialCreateInput = {
+    username: "qwe",
+    password: "qwe",
+  };
+  const credential = await prisma.credential.create({ data: credentialData });
+  console.log(credential);
+  const accountData: Prisma.AccountCreateInput = {
+    user: { connect: { id: user.id } },
+    provider: "credentials",
+    type: "credentials",
+    providerAccountId: credential.id,
+  };
+  const account = await prisma.account.create({ data: accountData });
+  console.log(account);
 }
 
 main();
