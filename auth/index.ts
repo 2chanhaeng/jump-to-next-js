96552aa311be16f236ba0b8f4422d52fee02ee72
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-19 19:09:35 +0900

    로그인 및 비로그인 별 접근 제한

--- auth/index.ts
+++ auth/index.ts
@@ -1,6 +1,19 @@
 import NextAuth from "next-auth";
 import providers from "@/providers";
 
-export const { handlers, signOut } = NextAuth({
+export const { handlers, signOut, auth } = NextAuth({
   providers,
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isLoginNeeded = nextUrl.pathname.startsWith("/questions/new");
+      if (isLoginNeeded) return isLoggedIn;
+      const isLogoutNeeded =
+        nextUrl.pathname.startsWith("/api/auth/signin") ||
+        nextUrl.pathname.startsWith("/signup");
+      if (isLogoutNeeded && isLoggedIn)
+        return Response.redirect(new URL("/", nextUrl));
+      return true;
+    },
+  },
 });
