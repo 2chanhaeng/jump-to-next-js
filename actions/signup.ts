de77b22546ea5ba7ac7eb1961b810406696da53f
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-20 17:52:18 +0900

    회원가입 서버 액션 작성

---
+++ actions/signup.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { Prisma, prisma } from "@/prisma";
+import { encrypt } from "@/utils/encrypt";
+
+export interface ErrorMessage {
+  message: string;
+}
+
+export async function signup(_: ErrorMessage, form: FormData) {
+  const username = form.get("username") as string;
+  const plain = form.get("password") as string;
+  const { salt, password } = encrypt(plain);
+  const credentialCreateInput = { data: { username, password, salt } };
+  const credential = await prisma.credential
+    .create(credentialCreateInput)
+    .catch(handleUserCreateError);
+  if ("message" in credential) return credential;
+  const user = await prisma.user.create({ data: {} });
+  const accountCreateInput = {
+    data: {
+      user: { connect: { id: user.id } },
+      provider: "credentials",
+      type: "credentials",
+      providerAccountId: credential.id,
+    },
+  };
+  await prisma.account.create(accountCreateInput);
+  return redirect("/api/auth/signin");
+}
+
+function handleUserCreateError(error: unknown): ErrorMessage {
+  return {
+    message:
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+        ? "username 중복"
+        : "알 수 없는 오류",
+  };
+}
