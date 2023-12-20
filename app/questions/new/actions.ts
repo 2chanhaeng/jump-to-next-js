3e8af5ec751f88be6e4f0f0f3cba4578cecb3b0f
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-20 18:18:33 +0900

    질문 생성 시 질문 목록 페이지 캐시 갱신

--- app/questions/new/actions.ts
+++ app/questions/new/actions.ts
@@ -2,11 +2,13 @@
 
 import { redirect } from "next/navigation";
 import { Prisma, prisma } from "@/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function createQuestion(form: FormData) {
   const subject = form.get("subject") as string;
   const content = form.get("content") as string;
   const data: Prisma.QuestionCreateInput = { subject, content };
   const { id } = await prisma.question.create({ data, select: { id: true } });
+  revalidatePath("/questions");
   redirect(`/questions/${id}`);
 }
