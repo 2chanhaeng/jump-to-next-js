1709a0dcca5abd4ed65272a6d594578637ea3ba8
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-20 15:36:41 +0900

    세션 인증 정보에 따른 NewAnswer 기능 구분

--- components/NewAnswer.tsx
+++ components/NewAnswer.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
 import { create } from "@/actions/answer";
+import { auth } from "@/auth";
 
-export function NewAnswer({ id }: { id: string }) {
-  return (
+export async function NewAnswer({ id }: { id: string }) {
+  const session = await auth();
+  return session ? (
     <form action={create.bind(null, id)}>
       <label>
         Comment
@@ -9,5 +12,9 @@ export function NewAnswer({ id }: { id: string }) {
       </label>
       <button type="submit">Add Comment</button>
     </form>
+  ) : (
+    <p>
+      댓글을 남기려면 <Link href="/api/auth/signin">로그인</Link>이 필요합니다.
+    </p>
   );
 }
