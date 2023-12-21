c2e1d804ca50002e55819c0542d7043a37e01465
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-21 17:19:10 +0900

    글 작성 시 사용자 추가

--- actions/answer.ts
+++ actions/answer.ts
@@ -1,12 +1,16 @@
 "use server";
 
-import { Prisma, prisma } from "@/prisma";
 import { revalidatePath } from "next/cache";
+import { auth } from "@/auth";
+import { Prisma, prisma } from "@/prisma";
 
 export async function create(id: string, form: FormData) {
+  const session = await auth();
+  if (!session || !session.user) return;
+  const user = { connect: { id: session.user.id } };
   const content = form.get("content") as string;
   const question = { connect: { id } };
-  const data: Prisma.AnswerCreateInput = { content, question };
+  const data: Prisma.AnswerCreateInput = { content, question, user };
   await prisma.answer.create({ data });
   revalidatePath(`/questions/${id}`);
 }
--- actions/question.ts
+++ actions/question.ts
@@ -1,13 +1,17 @@
 "use server";
 
-import { redirect } from "next/navigation";
-import { Prisma, prisma } from "@/prisma";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import { Prisma, prisma } from "@/prisma";
 
 export async function create(form: FormData) {
+  const session = await auth();
+  if (!session || !session.user) return;
+  const user = { connect: { id: session.user.id } };
   const subject = form.get("subject") as string;
   const content = form.get("content") as string;
-  const data: Prisma.QuestionCreateInput = { subject, content };
+  const data: Prisma.QuestionCreateInput = { subject, content, user };
   const { id } = await prisma.question.create({ data, select: { id: true } });
   revalidatePath("/questions");
   redirect(`/questions/${id}`);
