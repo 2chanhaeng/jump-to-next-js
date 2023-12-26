ea7862139b852bdaab5a321f09542ea879e55ab8
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-27 05:52:02 +0900

    질문 삭제 및 제거 서버 액션 작성

--- actions/answer.ts
+++ actions/answer.ts
@@ -14,3 +14,33 @@ export async function create(id: string, form: FormData) {
   await prisma.answer.create({ data });
   revalidatePath(`/questions/${id}`);
 }
+
+export async function destroy(id: string) {
+  const session = await auth();
+  if (!session?.user) return;
+  const userId = session.user.id;
+  const where = { ownership: { userId, id } };
+  const select = { question: { select: { id: true } } };
+  const questionId = await prisma.answer
+    .delete({ where, select })
+    .then(({ question: { id } }) => id)
+    .catch(() => null);
+  if (!questionId) return;
+  revalidatePath(`/questions/${questionId}`);
+}
+
+export async function update(form: FormData) {
+  const session = await auth();
+  if (!session?.user) return;
+  const userId = session.user.id;
+  const id = form.get("id") as string;
+  const where = { ownership: { id, userId } };
+  const data = { content: form.get("content") as string };
+  const select = { question: { select: { id: true } } };
+  const questionId = await prisma.answer
+    .update({ where, data, select })
+    .then(({ question: { id } }) => id)
+    .catch(() => null);
+  if (!questionId) return;
+  revalidatePath(`/questions/${questionId}`);
+}
