218791d9d21558c10d1b8274019f64147207aacc
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-29 02:05:51 +0900

    회원가입 페이지 최상단 페이지 main 태그 추가

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,17 +7,19 @@ export default function SignupPage() {
   const [state, dispatchSignup] = useFormState(signup, { message: "" });
 
   return (
-    <form action={dispatchSignup}>
-      <label>
-        Username
-        <input name="username" />
-      </label>
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <p>{state.message}</p>
-      <button type="submit">Sign up</button>
-    </form>
+    <main>
+      <form action={dispatchSignup}>
+        <label>
+          Username
+          <input name="username" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" />
+        </label>
+        <p>{state.message}</p>
+        <button type="submit">Sign up</button>
+      </form>
+    </main>
   );
 }
